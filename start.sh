960dc4f5e238f7d64f505fa9740c25a9896cb8ec
Hemen Showkati <h.showkati@gmail.com>
2021-11-09 21:10:10 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@ cd /app/bundle
 
 for i in `seq 1 30`; do
     node main.js &&
-    s=$$? && break || s=$$?;
+    s=$? && break || s=$?;
     echo \"Tried $$i times. Waiting 5 secs...\";
     sleep 5;
-done; (exit $$s)
+done; (exit $s)
